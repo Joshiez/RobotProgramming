1da8afdb10d02215f5990199e79dd7752f7139a4
Joshiez <jhuburn@gmail.com>
2014-01-25 23:15:13 +0000

    Update PaternChange.java

--- PaternChange.java
+++ PaternChange.java
@@ -1,11 +1,13 @@
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.robotics.navigation.DifferentialPilot;
-import lejos.util.Delay;
+import lejos.nxt.ButtonListener;
+import lejos.nxt.LCD;
 
 public class PaternChange {
 
-	DifferentialPilot pilot;
+	static DifferentialPilot pilot;
+	static int paternValue = 1;
 
 	public static void main(String[] args) {
 
@@ -13,26 +15,62 @@ public class PaternChange {
 
 		viktor.pilot = new DifferentialPilot(2.1f, 4.6f, Motor.B, Motor.C,
 				false);
-
+		
+		
 		Button.waitForAnyPress();
 
-		while (Button.ESCAPE.isUp()) {
-			viktor.go();
-		}
+		Button.ENTER.addButtonListener(new ButtonListener() {
+			public void buttonPressed(Button b) {
+				paternChange();
+			}
+
+			public void buttonReleased(Button b) {
+				runPatern();
+			}
+		}); 
+
+		Button.ESCAPE.waitForPressAndRelease();
 	}
 
-	@SuppressWarnings("deprecation")
-	public void go() {
-		while (!Button.ENTER.isPressed()) {
-			pilot.travel(5, true);
-			pilot.rotate(90, true);
+	public static void runPatern() {
+
+		if (paternValue == 1) {
+			System.out.println("1");
+			while (Button.ENTER.isUp()) {
+				pilot.rotate(90,true);
+				pilot.travel(5);
+			}
+		} else {
+			System.out.println("2");
+			while (Button.ENTER.isUp()) {
+				pilot.rotate(-90,true);
+				pilot.travel(5);
+			}
 		}
 
-		Delay.msDelay(1000);
-
-		while (!Button.ENTER.isPressed()) {
-			pilot.travel(5, true);
-			pilot.rotate(-90, true);
+	}
+	
+	public static void runPatern1(){
+		System.out.println("1");
+		while(Button.ESCAPE.isUp()){
+			pilot.rotate(90);
+			pilot.travel(5);
+		}
+	}
+	
+	public static void runPatern2(){
+		System.out.println("2");
+		while(Button.ENTER.isUp()){
+			pilot.rotate(-90);
+			pilot.travel(5);
+		}
+	}
+	
+	public static void paternChange() {
+		if (paternValue == 1) {
+			paternValue = 2;
+		} else {
+			paternValue = 1;
 		}
 	}
 
